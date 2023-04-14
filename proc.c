2bd7db3796e1504397bcb479eb0a9a883e267028
Sunny <sunnybaby28@gmail.com>
2023-04-14 11:58:29 +0800

    update

--- proc.c
+++ proc.c
@@ -9,7 +9,7 @@
 
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
 
-static struct proc_ops ops = {
+static struct file_operations ops = {
 	.proc_read = proc_read,
 };
 
